ab5bca84e2986923c408a9f45166997cafd9cc05
oscarsidsa81 <ovilches@sid-sa.com>
2026-01-14 14:17:29 +0100

    revisión para primer módulo a producción

--- models/bonds_order.py
+++ models/bonds_order.py
@@ -344,7 +344,8 @@ class BondsOrder ( models.Model ) :
 
 
 class SaleQuotationsBonds(models.Model):
-    _inherit = "sale.quotations"
+    _name = "sale.quotations"
+    _inherit = ["sale.quotations", "mail.thread", "mail.activity.mixin"]
     _parent_store = True  # activa parent_path (solo si tienes parent_path en el modelo)
     _parent_name = "parent_id"
 
